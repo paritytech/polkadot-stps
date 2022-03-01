479ad2f21c2771f5140d800cfbd3a2e22c44c097
Bernardo A. Rodrigues <bernardo@parity.io>
2022-02-28 21:32:11 -0300

    zombienet.sh print_help

--- zombienet.sh
+++ zombienet.sh
@@ -3,6 +3,18 @@
 POLKADOT_V=v0.9.17-rc4
 ZOMBIENET_V=v1.2.14
 
+print_help() {
+  echo "🧟 Zombienet Ecosystem Performance Optimizations 🦾"
+  echo ""
+  echo "we are about to spin a polkadot relay chain with a parachain node with extrinsics to be tested against."
+  echo "first, create a pallet for your team, including the extrinsics you want to run tests for."
+  echo "make sure you read zombienet specs from it's official repo: https://github.com/paritytech/zombienet"
+  echo "write the zombienet test specifications under the tests directory"
+  echo "then, call this script with the following parameters:"
+  echo "$ ./zombienet.sh test <team_name> <test_name>"
+  echo ""
+}
+
 fetch_zombienet() {
   if [ ! -s bin/zombienet-linux ]; then
     echo "fetching zombienet executable..."
@@ -38,5 +50,13 @@ run_ecosystem_benchmarks() {
 
   ./bin/zombienet-linux -p native test tests/examples/0001-simple-network.feature
 }
+
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         print_help
+         exit;;
+   esac
+done
 
 run_ecosystem_benchmarks
