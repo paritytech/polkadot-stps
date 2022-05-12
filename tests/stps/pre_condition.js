90a16ef028d58e5eada438fcd900ce0f4661aeb5
Bernardo A. Rodrigues <bernardo@parity.io>
2022-05-12 15:42:47 -0300

    update pre_condition

--- tests/stps/pre_condition.js
+++ tests/stps/pre_condition.js
@@ -1,6 +1,5 @@
 const polkadotApi = require("@polkadot/api");
 const { Keyring } = require('@polkadot/keyring');
-const { MNEMONICS } = require("./constants");
 
 async function connect(apiUrl, types) {
 	const provider = new polkadotApi.WsProvider(apiUrl);
@@ -10,34 +9,30 @@ async function connect(apiUrl, types) {
 }
 
 // Checks pre conditions.
-// - Check that all genesis accounts have the expected balance and nonce.
+// - Check that first and last genesis accounts have the expected balance and nonce.
 async function run(nodeName, networkInfo, args) {
-	const NUM_EXT = parseInt(args);
+	const NUM_ACCOUNTS = parseInt(args);
 	const { wsUri, userDefinedTypes } = networkInfo.nodesByName[nodeName];
 	const api = await connect(wsUri, userDefinedTypes);
 
-	// Check all hard-coded accounts.
-	await Promise.all(MNEMONICS.map(async (menmonic) => {
-		check_account(api, menmonic, NUM_EXT);
-	}));
+	check_account(api, "//Sender/0");
+	check_account(api, "//Sender/" + (NUM_ACCOUNTS-1).toString());
 }
 
-async function check_account(api, menmonic, NUM_EXT) {
+async function check_account(api, menmonic) {
 	let existential = api.consts.balances.existentialDeposit;
 	const keyring = new Keyring({ type: 'sr25519' });
 	const acc = keyring.addFromUri(menmonic);
-	await check_address(api, acc.address, existential, NUM_EXT);
+	await check_address(api, acc.address, existential);
 }
 
 // Checks that the address has the correct balance and nonce.
-async function check_address(api, addr, existential, NUM_EXT) {
+async function check_address(api, addr, existential) {
 	let { data: { free }, nonce } = await api.query.system.account(addr);
 	if (nonce != 0) {
 		throw new Error(`Address has a non-zero nonce: ${nonce}`);
 	}
-	if (free < 10000000000000000)
-		throw new Error(`Address has less than the initial balance: ${free}`);
-	if (free < existential * NUM_EXT * 1.1 /* 10% for fees */)
+	if (free < existential * 1.1 /* 10% for fees */)
 		throw new Error(`Address has insufficient funds: ${free}`);
 }
 
