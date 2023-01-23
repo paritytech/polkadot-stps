8327e702d7d12da97ae443c0ac23b7a3091b7901
bernardo <bernardo@parity.io>
2023-01-23 13:15:01 -0300

    fix sender.sh

--- tests/kubernetes/sender.sh
+++ tests/kubernetes/sender.sh
@@ -2,8 +2,8 @@ set -e
 SENDER_EXECUTABLE="https://storage.googleapis.com/zombienet-db-snaps/stps/sender"
 
 if [ ! -s sender ]; then
-    curl -o sender $SENDER_EXECUTABLE
-    chmod +x sender
+    /cfg/curl -o /tmp/sender $SENDER_EXECUTABLE
+    chmod +x /tmp/sender
 fi
 
-./sender --node-url ws://127.0.0.1:9944 --sender-index $1 --total-senders $2 -n $3
+/tmp/sender --node-url ws://127.0.0.1:9944 --sender-index $1 --total-senders $2 -n $3
