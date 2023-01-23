f2b259339d83b87c1bb8bbafc578ab75d3cc4302
bernardo <bernardo@parity.io>
2023-01-23 10:56:45 -0300

    sender.sh curl

--- tests/kubernetes/sender.sh
+++ tests/kubernetes/sender.sh
@@ -4,7 +4,7 @@ set -e
 SENDER_EXECUTABLE="https://storage.googleapis.com/zombienet-db-snaps/stps/sender"
 
 if [ ! -s sender ]; then
-    wget --quiet https://storage.googleapis.com/zombienet-db-snaps/stps/sender
+    curl -o sender https://storage.googleapis.com/zombienet-db-snaps/stps/sender
     chmod +x sender
 fi
 
