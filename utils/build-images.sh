25657f3bf73fce7ca3b12f13ea8c4879747e0636
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2023-04-26 10:57:57 +0000

    Fix path

--- utils/build-images.sh
+++ utils/build-images.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build tps image
-docker build -f dockerfiles/Dockerfile.tps -t stps-tps:latest .
+docker build -f utils/dockerfiles/Dockerfile.tps -t stps-tps:latest .
 
 # Build sender image
-docker build -f dockerfiles/Dockerfile.sender -t stps-sender:latest .
+docker build -f utils/dockerfiles/Dockerfile.sender -t stps-sender:latest .
 
 # Build funder image
-docker build -f dockerfiles/Dockerfile.funder -t stps-funder:latest .
+docker build -f utils/dockerfiles/Dockerfile.funder -t stps-funder:latest .
