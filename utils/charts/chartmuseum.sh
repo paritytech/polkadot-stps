42be7cd8b0c1145fce694821bb572af325f39a43
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2023-05-04 10:47:26 +0000

    Add chartmuseum helm repo

--- utils/charts/chartmuseum.sh
+++ utils/charts/chartmuseum.sh
@@ -5,11 +5,13 @@ docker run --rm -it -d \
   -e DEBUG=1 \
   -e STORAGE=local \
   -e STORAGE_LOCAL_ROOTDIR=/charts \
-  --name helm-repository \
+  --name chartmuseum \
   -v $(pwd)/charts:/charts \
   ghcr.io/helm/chartmuseum:v0.15.0
 
-  sudo chmod 777 charts
+sudo chmod 777 charts
 
-  cd sender && helm package . && curl --data-binary "@stps-sender-0.1.0.tgz" http://localhost:8080/api/charts && cd ..
-  cd tps && helm package . && curl --data-binary "@stps-tps-scraper-0.1.0.tgz" http://localhost:8080/api/charts && cd ..
+helm repo add chartmuseum http://localhost:8080
+
+cd sender && helm package . && curl --data-binary "@stps-sender-0.1.0.tgz" http://localhost:8080/api/charts && cd ..
+cd tps && helm package . && curl --data-binary "@stps-tps-scraper-0.1.0.tgz" http://localhost:8080/api/charts && cd ..
