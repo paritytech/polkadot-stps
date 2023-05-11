1bf5f08f06517cacba1415af488585451b04c420
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2023-05-11 10:34:23 +0000

    * Add parachain monitor crate * Initial port of parachain monitor to tps crate

--- utils/tps/src/main.rs
+++ utils/tps/src/main.rs
@@ -2,14 +2,28 @@ use clap::Parser;
 use log::*;
 use tokio::time::{sleep, Duration};
 use utils::{connect, runtime, Api, Error};
+use futures_util::StreamExt;
+use parity_scale_codec::{Decode, Encode};
+use polkadot_primitives::{
+	v4::{CandidateDescriptor, CandidateReceipt},
+	Hash,
+};
+use subxt::ext::scale_decode::DecodeAsType;
+use subxt::utils::H256;
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 /// util program to count TPS
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-	/// Node URL
+	/// Relay node URL
 	#[arg(long)]
-	node_url: String,
+	relay_url: String,
+
+	/// Collator node URL
+	#[arg(long)]
+	collator_url: Option<String>,
 
 	/// Total number of senders
 	#[arg(long)]
@@ -19,19 +33,122 @@ struct Args {
 	#[arg(long)]
 	num: usize,
 
-	/// Start scraping from genesis
+	/// Whether to subscribe to blocks from genesis or not.
 	#[arg(short, long, default_value_t = false)]
 	genesis: bool,
+
+	/// Whether to monitor relay-chain, or para-chain finality.
+	/// If set to para-chain, will subscribe to CandidateIncluded events and
+	/// Transfer events concurrently with two different clients; one for the relaychain node,
+	/// the other for the collator node.
+	#[arg(short, long, default_value_t = false)]
+	para_finality: bool,
+
+	/// Default parablock time set to 12s for sync-backing.
+	#[arg(short, long, default_value_t = 12.0)]
+	default_parablock_time: f32
+}
+
+async fn count_transfers(
+	para_api: &Api,
+	mut rx: Receiver<H256>,
+	default_parablock_time: u64,
+) -> Result<(), Error> {
+	let storage_timestamp_storage_addr = runtime::storage().timestamp().now();
+	let mut trx_in_parablock = 0;
+	while let Some(para_head) = rx.recv().await {
+		info!("TPS Counter ===> Received ParaHead: {:?}", para_head);
+		let parablock = para_api.blocks().at(Some(para_head)).await?;
+		let parabody = parablock.body().await?;
+		let parablock_number = parablock.number();
+		let previous_parablock_number = parablock_number - 1;
+		let maybe_previous_parablock_hash =
+			para_api.rpc().block_hash(Some(previous_parablock_number.into())).await?;
+
+		// Need to handle the case where we cannot get the previous parablock timestamp
+		let parablock_time = match maybe_previous_parablock_hash {
+			Some(hash) => {
+				let parablock_timestamp = para_api
+					.storage()
+					.fetch(&storage_timestamp_storage_addr, Some(para_head))
+					.await?
+					.unwrap();
+				let previous_parablock_timestamp = para_api
+					.storage()
+					.fetch(&storage_timestamp_storage_addr, Some(hash))
+					.await?
+					.unwrap();
+				let time_diff = parablock_timestamp - previous_parablock_timestamp;
+				info!("TPS Counter ===> Parablock time estimated at: {:?}", time_diff);
+				time_diff
+			},
+			// Assume default if unable to get the previous parablock from parablock number
+			None => {
+				warn!(
+					"TPS Counter ===> Assuming default parablock time of: {:?}",
+					default_parablock_time
+				);
+				Duration::as_secs_f64(&Duration::new(default_parablock_time, 0)) as u64
+			},
+		};
+		for extrinsic in parabody.extrinsics() {
+			for events in extrinsic.events().await {
+				for event in events.iter() {
+					let evt = event?;
+					let variant = evt.variant_name();
+					if variant == "Transfer" {
+						trx_in_parablock += 1;
+					}
+				}
+			}
+		}
+		// Print to stdout and reset counter
+		info!(
+			"TPS Counter ===> Counted {} TPS in ParaHead: {:?}",
+			trx_in_parablock / parablock_time,
+			para_head
+		);
+	}
+	trx_in_parablock = 0;
+	Ok(())
+}
+
+async fn subscribe(relay_api: &Api, tx: Sender<H256>) -> Result<(), Error> {
+	// Subscribe to all finalized blocks:
+	let mut blocks_sub = relay_api.blocks().subscribe_finalized().await?;
+	// For each block, check if the CandidateIncluded extrinsic occurs
+	while let Some(block) = blocks_sub.next().await {
+		let block = block?;
+		let body = block.body().await?;
+		for ext in body.extrinsics() {
+			let events = ext.events().await?;
+			for evt in events.iter() {
+				let evt = evt?;
+				let event_name = evt.variant_name();
+				if event_name == "CandidateIncluded" {
+					// If the CandidateIncluded event occurs, we need to get the CandidateDescriptor by decoding bytes
+					let mut values = evt.field_bytes();
+					let candidate_receipt = CandidateReceipt::<Hash>::decode(&mut values)?;
+					let descriptor = candidate_receipt.descriptor();
+					info!(
+						"ParaBlock Subscriber ===> New ParaHead: {:?} for ParaId: {:?}",
+						descriptor.para_head, descriptor.para_id
+					);
+					tx.send(descriptor.para_head).await?;
+				}
+			}
+		}
+	}
+	Ok(())
 }
 
 /// we loop over blocks and count the number of transfers in each block
 /// we take the timestamp difference between block X and block X-1
 pub async fn calc_tps(api: &Api, n: usize, genesis: bool) -> Result<(), Error> {
 	let storage_timestamp_storage_addr = runtime::storage().timestamp().now();
-
 	// do we start from genesis, or latest finalized block?
 	let (mut block_number_x, mut block_timestamp_x_minus_one) = match genesis {
-		true => {
+		false => {
 			let block_hash_genesis = api
 				.rpc()
 				.block_hash(Some(1u32.into()))
@@ -44,7 +161,7 @@ pub async fn calc_tps(api: &Api, n: usize, genesis: bool) -> Result<(), Error> {
 				.unwrap();
 			(2, block_timestamp_genesis)
 		},
-		false => {
+		true => {
 			let block_hash_x = api.rpc().finalized_head().await?;
 			let block_header_x = api
 				.rpc()
@@ -116,12 +233,32 @@ async fn main() -> Result<(), Error> {
 	);
 
 	let args = Args::parse();
+	let para_finality = args.para_finality;
 
-	let n_tx_truncated = (args.num / args.total_senders) * args.total_senders;
+	match para_finality {
+		true => {
+			// Don't need to process many messages concurrently as throughput depends on parablock times
+			let (tx, mut rx) = channel::<H256>(10);
+			let validator_url = args.validator_url;
+			let collator_url = args.collator_url;
+			let relay_api = connect(&validator_url).await?;
+			let para_api = connect(&collator_url).await?;
 
-	let api = connect(&args.node_url).await?;
+			info!("Spawning new async thread for subscribing to relay chain blocks.");
+			tokio::spawn(async move {
+				subscribe(&relay_api, tx).await;
+			});
 
-	calc_tps(&api, n_tx_truncated, args.genesis).await?;
+			info!("Counting Transfer events from main thread");
+			count_transfers(&para_api, rx, args.default_parablock_time).await?;
+		},
+		
+		false => {
+			let n_tx_truncated = (args.num / args.total_senders) * args.total_senders;
+			let api = connect(&args.node_url).await?;
+			calc_tps(&api, n_tx_truncated, args.genesis).await?;
+		}
+	}
 
 	Ok(())
 }
