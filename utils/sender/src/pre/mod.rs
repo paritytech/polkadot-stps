b4826a1b870be8916a501e9624f165f8a638b9f2
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2023-05-19 16:23:19 +0000

    Add --para-finality argument, and start match block in pre-checks

--- utils/sender/src/pre/mod.rs
+++ utils/sender/src/pre/mod.rs
@@ -6,7 +6,7 @@ use subxt::{tx::PairSigner, PolkadotConfig};
 use utils::{runtime, Api, Error, DERIVATION};
 
 /// Check pre-conditions of accounts attributed to this sender
-pub async fn pre_conditions(api: &Api, i: usize, n: usize) -> Result<(), Error> {
+pub async fn pre_conditions(api: &Api, i: usize, n: usize, para_finality: bool) -> Result<(), Error> {
 	info!(
 		"Sender {}: checking pre-conditions of accounts {}{} through {}{}",
 		i,
@@ -22,35 +22,38 @@ pub async fn pre_conditions(api: &Api, i: usize, n: usize) -> Result<(), Error> 
 		let signer: PairSigner<PolkadotConfig, SrPair> = PairSigner::new(pair);
 		let account = signer.account_id();
 		info!("Checking account: {}", account);
-		check_account(&api, account).await?;
+		check_account(&api, account, para_finality).await?;
 	}
 
 	Ok(())
 }
 
 /// Check account nonce and free balance
-async fn check_account(api: &Api, account: &AccountId32) -> Result<(), Error> {
+async fn check_account(api: &Api, account: &AccountId32, para_finality: bool) -> Result<(), Error> {
 	let ext_deposit_addr = runtime::constants().balances().existential_deposit();
 	let ext_deposit = api.constants().at(&ext_deposit_addr)?;
+	let account_state_storage_addr = runtime::storage().system().account(account);	
+	
+	match para_finality {
+		true => {
+			// TODO: Need to check accounts in parablock
+		},
+		false => {
+			let finalized_head_hash = api.rpc().finalized_head().await?;
+			let account_state =
+				api.storage().fetch(&account_state_storage_addr, Some(finalized_head_hash)).await?.unwrap();
 
-	let genesis = 0u32;
-	let genesis_hash = api.rpc().block_hash(Some(genesis.into())).await?;
+			if account_state.nonce != 0 {
+				panic!("Account has non-zero nonce");
+			}
 
-	// let account_state = runtime::storage().system().account(account, genesis_hash).await?;
-	let account_state_storage_addr = runtime::storage().system().account(account);
-	let account_state =
-		api.storage().fetch(&account_state_storage_addr, genesis_hash).await?.unwrap();
-
-	if account_state.nonce != 0 {
-		panic!("Account has non-zero nonce");
+			if (account_state.data.free as f32) < ext_deposit as f32 * 1.1
+			/* 10% for fees */
+			{
+				// 10% for fees
+				panic!("Account has insufficient funds");
+			}
+		}
 	}
-
-	if (account_state.data.free as f32) < ext_deposit as f32 * 1.1
-	/* 10% for fees */
-	{
-		// 10% for fees
-		panic!("Account has insufficient funds");
-	}
-
 	Ok(())
 }
